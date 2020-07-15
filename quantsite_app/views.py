e3a74a729f08d76ff810c8dd89f846e5f92611e3
Haziq <haziq.usman98@gmail.com>
2020-07-15 11:12:57 +0500

    django app

--- quantsite_app/views.py
+++ quantsite_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+from rest_framework.decorators import api_view, permission_classes
 
-# Create your views here.
+@api_view(['GET'])
+def index(request):
+    template = loader.get_template("quantsite_app/index.html")
+    return HttpResponse(template.render())
